737e9462da0c40bc58260ed78177b92a305dbf7c
Marco Paspuel <marco.paspuel@helsing.ai>
2022-02-20 12:29:59 +0100

    feat: add first draft of the aks tf template

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,3 +23,31 @@ module "resource_group" {
   location             = var.aks_rg_location
   project              = var.project
 }
+
+resource "random_pet" "prefix" {}
+
+resource "azurerm_kubernetes_cluster" "default" {
+  name                = "${random_pet.prefix.id}-aks"
+  location            = var.aks_rg_location
+  resource_group_name = var.aks_rg_name
+  dns_prefix          = "${random_pet.prefix.id}-k8s"
+
+  default_node_pool {
+    name            = "default"
+    node_count      = 2
+    vm_size         = "Standard_D2_v2"
+    os_disk_size_gb = 30
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  role_based_access_control {
+    enabled = true
+  }
+
+  tags = {
+    environment = "Demo"
+  }
+}
